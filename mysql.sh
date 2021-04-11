db4bee81f5a02743870698ba85db075410194801
paulconnor <paul.connor21@gmail.com>
2021-04-12 08:59:28 +1000

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
-DB_USER='pccu'
-DB_PASSWD='SYMCdemo123!'
-DB_NAME='pccu'
-DB_HOST='mysql.iamdemo.broadcom.com'
+DB_USER='<xxx>'
+DB_PASSWD='<yyy>'
+DB_NAME='<db>'
+DB_HOST='<URL>'
 
 #    TRUNCATE TABLE evaluateRiskResponse;
 mysql -h $DB_HOST --user=$DB_USER --password=$DB_PASSWD $DB_NAME
