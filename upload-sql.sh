ad2f4319024d33d6e0effb137caf3d681d3646ec
paulconnor <paul.connor21@gmail.com>
2021-04-12 09:01:30 +1000

    Update upload-sql.sh

--- upload-sql.sh
+++ upload-sql.sh
@@ -5,10 +5,10 @@ export OUTPUT=$1
 
 echo "Uploading processed data to MySQL"
 
-DB_USER='pccu'
-export MYSQL_PWD='SYMCdemo123!'
-DB_NAME='pccu'
-DB_HOST='mysql.iamdemo.broadcom.com'
+DB_USER='db'
+export MYSQL_PWD='password'
+DB_NAME='db'
+DB_HOST='host'
 
 mysql -h $DB_HOST --user=$DB_USER $DB_NAME << EOF
 load data local infile '${OUTPUT}' into table riskResponse fields terminated by ',' lines terminated by '\n';
